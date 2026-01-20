0d1bdf58883c97e342d9b1699cdf43848c47460c
Kam <13855357+opelton@users.noreply.github.com>
2026-01-20 15:08:15 -0800

    organization
    core folder was getting big

--- Assets/_Scripts/Core/Enums/CardinalEnum.cs
+++ Assets/_Scripts/Core/Enums/CardinalEnum.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 namespace Potato.Core
 {
     [CreateAssetMenu(menuName = "ScriptableObjects/Enums/Cardinal")]
-    public class CardinalEnum : ScriptableEnum { }
+    public class CardinalEnum : ScriptableEnum
+    {
+        public Vector2 Direction;
+    }
 
 }
