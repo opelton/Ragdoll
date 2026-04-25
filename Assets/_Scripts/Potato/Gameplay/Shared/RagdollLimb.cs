409710b9824a6d78cd92836f9432fa92aa81aaf2
Kam <13855357+opelton@users.noreply.github.com>
2026-04-24 21:56:37 -0700

    more tweaks
    player camera doesn't clip through walls anymore, tuned ragdoll to glitch out less (still does it a bunch)

--- Assets/_Scripts/Potato/Gameplay/Shared/RagdollLimb.cs
+++ Assets/_Scripts/Potato/Gameplay/Shared/RagdollLimb.cs
@@ -3,30 +3,27 @@ using UnityEngine.Events;
 
 namespace Potato.Gameplay
 {
-    [RequireComponent(typeof(Rigidbody), typeof(Collider))]
+    [RequireComponent(typeof(Rigidbody))]
     public class RagdollLimb : MonoBehaviour
     {
         public UnityAction onAttacked;
 
         private Rigidbody _rb;
-        private Collider _collider;
 
         void Awake()
         {
             _rb = GetComponent<Rigidbody>();
-            _collider = GetComponent<Collider>();
         }
 
         public void SetRagdoll(bool enabled)
         {
             _rb.isKinematic = !enabled;
-            _collider.enabled = enabled;
         }
 
         public void AttackLimb(float damage, Vector3 point, Vector3 direction, GameObject attacker)
         {
             onAttacked?.Invoke();
-            var force = 1000f;
+            var force = 1500;
             _rb.AddForceAtPosition(direction * force, point);
         }
     }
