d35c9379acd0384d9ca246c73366bd62e65e3a87
Kam <13855357+opelton@users.noreply.github.com>
2026-04-26 01:15:44 -0700

    extremely basic dismemberment
    limbs just detach if shot, needs some spicing up

--- Assets/_Scripts/Potato/Gameplay/Shared/RagdollLimb.cs
+++ Assets/_Scripts/Potato/Gameplay/Shared/RagdollLimb.cs
@@ -6,8 +6,9 @@ namespace Potato.Gameplay
     [RequireComponent(typeof(Rigidbody))]
     public class RagdollLimb : MonoBehaviour
     {
+        [SerializeField] private bool dismembering = true;
+        [SerializeField] private float knockbackForce = 1500f;
         public UnityAction onAttacked;
-
         private Rigidbody _rb;
 
         void Awake()
@@ -23,8 +24,20 @@ namespace Potato.Gameplay
         public void AttackLimb(float damage, Vector3 point, Vector3 direction, GameObject attacker)
         {
             onAttacked?.Invoke();
-            var force = 1500;
+            var force = damage * knockbackForce;
             _rb.AddForceAtPosition(direction * force, point);
+
+            if (dismembering)
+            {
+                if (transform.parent != null)
+                    transform.parent.DetachChildren();
+
+                if (TryGetComponent(out CharacterJoint joint))
+                {
+                    joint.connectedBody = null;
+                    Destroy(joint);
+                }
+            }
         }
     }
 }
