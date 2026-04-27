83edc7553b9e63773a5cd14418f7f0443bf54f6b
Kam <13855357+opelton@users.noreply.github.com>
2026-04-27 14:10:31 -0700

    dismemberment tuning
    forces now carry up the chain before the dismemberment is applied, added more control values

--- Assets/_Scripts/Potato/Gameplay/Shared/RagdollLimb.cs
+++ Assets/_Scripts/Potato/Gameplay/Shared/RagdollLimb.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -6,8 +7,12 @@ namespace Potato.Gameplay
     [RequireComponent(typeof(Rigidbody))]
     public class RagdollLimb : MonoBehaviour
     {
+        static readonly float kHitForce = 1500f;
+        [Header("Basic response")]
+        [SerializeField] [Range(0f, 2f)] private float hitForceModifier = 1f;
+        [SerializeField] [Range(0f, 1f)] private float severVelocityModifier = .5f;
         [SerializeField] private bool dismembering = true;
-        [SerializeField] private float knockbackForce = 1500f;
+
         public UnityAction onAttacked;
         private Rigidbody _rb;
 
@@ -21,25 +26,29 @@ namespace Potato.Gameplay
             _rb.isKinematic = !enabled;
         }
 
-        public void AttackLimb(float damage, Vector3 point, Vector3 direction, GameObject attacker)
+        IEnumerator BeginSevering()
+        {
+            // wait one physics update so forces apply before detaching
+            yield return new WaitForFixedUpdate();
+
+            if (transform.parent != null)
+                transform.parent.DetachChildren();
+
+            if (TryGetComponent(out CharacterJoint joint))
+                Destroy(joint);
+
+            _rb.velocity *= severVelocityModifier;
+            dismembering = false;
+        }
+
+        public void AttackLimb(float damage, Vector3 point, Vector3 direction, GameObject _)
         {
             onAttacked?.Invoke();
-            var force = damage * knockbackForce;
+            var force = damage * kHitForce * hitForceModifier;
             _rb.AddForceAtPosition(direction * force, point);
 
             if (dismembering)
-            {
-                if (transform.parent != null)
-                    transform.parent.DetachChildren();
-
-                if (TryGetComponent(out CharacterJoint joint))
-                {
-                    joint.connectedBody = null;
-                    Destroy(joint);
-                }
-
-                dismembering = false;
-            }
+                StartCoroutine(BeginSevering());
         }
     }
 }
